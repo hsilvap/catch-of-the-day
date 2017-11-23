8faea19c06c47a290c6a718a9001459fe916c4b8
Homero Silva <hsilvap@me.com>
2017-11-23 17:24:19 -0600

    animations and add or delete items

--- src/components/App.js
+++ src/components/App.js
@@ -11,8 +11,11 @@ class App extends React.Component {
         super();
 
         this.addFish = this.addFish.bind(this);
+        this.updateFish = this.updateFish.bind(this);
+        this.removeFish = this.removeFish.bind(this);
         this.loadSamples = this.loadSamples.bind(this);
         this.addToOrder = this.addToOrder.bind(this);
+        this.removeFromOrder = this.removeFromOrder.bind(this);
         //initial state
         this.state = {
             fishes: {},
@@ -55,6 +58,18 @@ class App extends React.Component {
         this.setState({fishes: fishes});
     }
 
+    updateFish(key, updatedFish){
+        const fishes ={...this.state.fishes};
+        fishes[key] = updatedFish;
+        this.setState({fishes: fishes});
+    }
+
+    removeFish(key){
+        const fishes ={...this.state.fishes};
+        fishes[key] = null;
+        this.setState({fishes});
+    }
+
     loadSamples(){
         this.setState({
             fishes:sampleFishes
@@ -70,6 +85,12 @@ class App extends React.Component {
         this.setState({order});
     }
     
+    removeFromOrder(key){
+        const order = {...this.state.order}
+        delete order[key];
+        this.setState({order})
+    }
+
     render(){
         return (
             <div className="catch-of-the-day">
@@ -83,8 +104,14 @@ class App extends React.Component {
                 fishes={this.state.fishes} 
                 order={this.state.order}
                 params={this.props.params}
+                removeFromOrder={this.removeFromOrder}
                 />
-                <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
+                <Inventory 
+                addFish={this.addFish} 
+                updateFish={this.updateFish}
+                removeFish={this.removeFish}
+                loadSamples={this.loadSamples} 
+                fishes={this.state.fishes}/>
             </div>
         )
     }
